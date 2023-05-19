1e748743a80aa57fff298debc074f4c17cb047be
vn!ba <itsmescorona@gmail.com>
2023-05-19 10:22:25 +0530

    Delete dist directory

--- dist/assets/about-c693ec9c.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as e,H as o}from"./index-a0fc2b71.js";const n=()=>{const s=a(),t=()=>s("/");return e.jsxs("div",{className:"tc",children:[e.jsx(o,{}),e.jsx("h2",{className:"f-2 white",children:"Contact me"}),e.jsx("button",{className:"hover-blue",onClick:t,children:"Home"})]})};export{n as default};
