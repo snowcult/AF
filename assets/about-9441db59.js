c8d7d0431d111c9618009af72cd36dc7e9c37bce
vniba <itsmescorona@gmail.com>
2023-05-19 06:58:05 +0000

    Deploying to gh-pages from @ snowcult/AF@83fe518788df5d49519d89f071840de78601fbdb 🚀

---
+++ assets/about-9441db59.js
@@ -0,0 +1,1 @@
+import{u as a,j as e,H as o}from"./index-61dcd4ba.js";const n=()=>{const s=a(),t=()=>s("/");return e.jsxs("div",{className:"tc",children:[e.jsx(o,{}),e.jsx("h2",{className:"f-2 white",children:"Contact me"}),e.jsx("button",{className:"hover-blue",onClick:t,children:"Home"})]})};export{n as default};
--- src/components/pages/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div>
-      <h2 className="yellow tc shadow-2 f-5">Loading....</h2>
-    </div>
-  );
-};
-
-export default Loader;
