d54c3edf233c8022f23f803e728463141aa53f54
zetoz <zetoxzx@gmail.com>
2023-05-19 14:34:46 +0530

    vc typo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import pwaConfig from 'pwa.config.js';
+import pwaConfig from './pwa.config.js';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pwaConfig],
