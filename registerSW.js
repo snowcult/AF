e19d2cf3927b7f5a4a1b760c518127fb4200318d
vniba <itsmescorona@gmail.com>
2023-05-19 10:42:30 +0000

    Deploying to gh-pages from @ snowcult/AF@8f4db14834205415a9cb8c4a5c77c817db89adb6 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AF/sw.js', { scope: '/AF/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
