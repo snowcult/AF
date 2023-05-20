fc444f6c08dc0342f3e126b2966a6992f272b2d9
vniba <itsmescorona@gmail.com>
2023-05-20 11:17:31 +0000

    Deploying to gh-pages from @ snowcult/AF@42e169fa6cd75b2d1f6b6cbf7dddf19f074038df 🚀

---
+++ assets/about-c45d0539.js
@@ -0,0 +1,2 @@
+import{u as a,j as e,H as o}from"./index-fd01ac0f.js";const n=()=>{const s=a(),t=()=>s("/");return e.jsxs("div",{className:"tc",children:[e.jsx(o,{}),e.jsx("h2",{className:"f-2 white",children:"Contact me"}),e.jsx("button",{className:"hover-blue",onClick:t,children:"Home"})]})};export{n as default};
+//# sourceMappingURL=about-c45d0539.js.map
