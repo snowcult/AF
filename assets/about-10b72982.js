8ed02160f08f6e364f110ecf157c12f92104a469
vniba <itsmescorona@gmail.com>
2023-05-22 14:40:58 +0000

    Deploying to gh-pages from @ snowcult/AF@c625f10285f0e9f3698d4af3eaa382c3486d2f80 🚀

--- assets/about-10b72982.js
+++
@@ -1,2 +0,0 @@
-import{u as a,j as e,H as o}from"./index-62395d3f.js";const n=()=>{const s=a(),t=()=>s("/");return e.jsxs("div",{className:"tc",children:[e.jsx(o,{}),e.jsx("h2",{className:"f-2 white",children:"Contact me"}),e.jsx("button",{className:"hover-blue",onClick:t,children:"Home"})]})};export{n as default};
-//# sourceMappingURL=about-10b72982.js.map
---
+++ assets/about-82eb700b.js
@@ -0,0 +1,2 @@
+import{u as a,j as e,H as o}from"./index-99a677f0.js";const n=()=>{const s=a(),t=()=>s("/");return e.jsxs("div",{className:"tc",children:[e.jsx(o,{}),e.jsx("h2",{className:"f-2 white",children:"Contact me"}),e.jsx("button",{className:"hover-blue",onClick:t,children:"Home"})]})};export{n as default};
+//# sourceMappingURL=about-82eb700b.js.map
